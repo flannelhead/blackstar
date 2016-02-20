3cb6f3ad483e94c8651fab1d8ae31cb794ef352b
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2016-02-20 12:14:13 +0200

    Christoffels with Repa

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,23 @@
 module Main where
 
-import GR
+import GR hiding (toList)
 import Criterion.Main
-
-makeChristoffels :: Int -> String
-makeChristoffels l = unlines (map row [0..3]) ++ "\n" where
-    row :: Int -> String
-    row j = concatMap (cell j) [0..3]
-
-    cell :: Int -> Int -> String
-    cell j k = show (christoffel schwarzschild ischwarzschild (l, j, k)
-        (FVect (0 :: Double) 2 (pi/2) 0)) ++ "\t"
-
-allChristoffels :: Double -> [Double]
-allChristoffels r = [ christoffel schwarzschild ischwarzschild (l, j, k)
-    (FVect 0 r (pi/2) 0) | l <- [0..3], j <- [0..3], k <- [0..3] ]
+import Data.Array.Repa hiding (map)
 
 main :: IO ()
-main = defaultMain [ bench "christoffels" $ nf allChristoffels 2 ]
---main = putStrLn $ concatMap makeChristoffels [0..3]
+main = benchmarkChristoffels
+-- main = printChristoffels
+
+benchmarkChristoffels :: IO ()
+benchmarkChristoffels = defaultMain [ bench "christoffels"
+                                      $ whnf (christoffels schwarz ischwarz)
+                                        (FVect (0 :: Double) 2 (pi/2) 0) ]
+
+printChristoffels :: IO ()
+printChristoffels = putStrLn $ unlines (map (show . toList) rows)
+    where chris = christoffels schwarz ischwarz (FVect (0 :: Double) 2 (pi/2) 0)
+          rows :: [Array U DIM1 Double]
+          rows = [ computeS $ slice chris (Any :. (l :: Int) :. All)
+                   | l <- [0..3] ]
+
 
--- src/GR.hs
+++ src/GR.hs
@@ -2,7 +2,10 @@
     Rank2Types #-}
 
 module GR where
+
 import Numeric.AD
+import Data.Array.Repa hiding (map, toList)
+import Data.Array.Repa.Repr.Unboxed
 
 data FourVector a = FVect a a a a
                     deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
@@ -14,25 +17,55 @@ comp 2 (FVect _ _ x _) = x
 comp 3 (FVect _ _ _ x) = x
 comp _ _ = 0
 
+toList :: FourVector a -> [a]
+toList (FVect a b c d) = [a, b, c, d]
+
 type Metric a = (Int, Int) -> FourVector a -> a
 
-schwarzschild :: Floating a => Metric a
-schwarzschild (0, 0) (FVect _ r  _ _) = 1 - 1/r
-schwarzschild (1, 1) (FVect _ r  _ _) = -1 / (1 - 1/r)
-schwarzschild (2, 2) (FVect _ r  _ _) = -r**2
-schwarzschild (3, 3) (FVect _ r th _) = -(r * sin th)**2
-schwarzschild (_, _) _ = 0
+symIndices :: [(Int, Int)]
+symIndices = [ (0, 0), (0, 1), (0, 2), (0, 3),
+                       (1, 1), (1, 2), (1, 3),
+                               (2, 2), (2, 3),
+                                       (3, 3) ]
 
-ischwarzschild :: Floating a => Metric a
-ischwarzschild (mu, nu) coords = if mu == nu
-                                   then 1 / schwarzschild (mu, nu) coords
+convIndex :: (Int, Int) -> Int
+convIndex (i, j) = if i > j then idx (j, i)
+                               else idx (i, j)
+    where idx (0, k) = k
+          idx (1, k) = 3 + k
+          idx (2, k) = 5 + k
+          idx (3, k) = 6 + k
+          idx _ = -1
+
+christoffel :: (Unbox a, Floating a) => Array U DIM2 a -> Array U DIM1 a
+                                     -> (Int, Int, Int) -> a
+christoffel dmetric imetric (l, j, k) = sum (map sumTerm [0..3]) / 2
+    where sumTerm r = (imetric ! (Z :. convIndex (l, r))) *
+             (  (dmetric ! (Z :. convIndex (r, j) :. k))
+              + (dmetric ! (Z :. convIndex (r, k) :. j))
+              - (dmetric ! (Z :. convIndex (j, k) :. r)) )
+
+christoffels :: (Unbox a, Floating a) => (forall b. Floating b => Metric b)
+                                      -> Metric a -> FourVector a
+                                      -> Array U DIM2 a
+christoffels metric imetric crd =
+    fromListUnboxed (Z :. (4 :: Int) :. (10 :: Int))
+    [ christoffel metricDerivatives inverseComponents (l, j, k)
+      | l <- [0..3], (j, k) <- symIndices ]
+    where
+        inverseComponents = fromListUnboxed (Z :. (10 :: Int))
+            [ imetric idx crd | idx <- symIndices ]
+        metricDerivatives = fromListUnboxed (Z :. (10 :: Int) :. (4 :: Int))
+            $ concat [ toList (grad (metric idx) crd) | idx <- symIndices ]
+
+schwarz :: Floating a => Metric a
+schwarz (0, 0) (FVect _ r  _ _) = 1 - 1/r
+schwarz (1, 1) (FVect _ r  _ _) = -1 / (1 - 1/r)
+schwarz (2, 2) (FVect _ r  _ _) = -r**2
+schwarz (3, 3) (FVect _ r th _) = -(r * sin th)**2
+schwarz (_, _) _ = 0
+
+ischwarz :: Floating a => Metric a
+ischwarz (mu, nu) coords = if mu == nu
+                                   then 1 / schwarz (mu, nu) coords
                                    else 0
-
-christoffel :: Floating a => (forall b. Floating b => Metric b) -> Metric a
-                             -> (Int, Int, Int) -> FourVector a -> a
-christoffel metric imetric (l, j, k) crd = sum (map sumTerm [0..3]) / 2
-    where sumTerm r = imetric (l, r) crd *
-             (comp k (grad (metric (r, j)) crd)
-              + comp j (grad (metric (r, k)) crd)
-              - comp r (grad (metric (j, k)) crd))
-
