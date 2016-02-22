c1f51b47cb8cf127290e8aa20f6ba1e4fd4ccc09
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2016-02-22 10:03:10 +0200

    remove out.png before writing

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
-import Geometry
-import Raytracer
+import System.Directory
 import Vision.Image
 import Vision.Image.Storage.DevIL
+
+import Geometry
+import Raytracer
 
 myScene :: Scene
 myScene = Scene { stepSize = 0.1
@@ -23,5 +25,6 @@ myCamera = Camera { position = [20, 0, 1]
 main :: IO ()
 main = do
     img <- computeP $ raytrace myScene
-    _ <- save Autodetect "out.png" ((convert img) :: RGB)
+    removeFile "out.png"
+    _ <- save PNG "out.png" ((convert img) :: RGB)
     return ()
