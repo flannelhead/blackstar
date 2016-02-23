d34884b12fff1c92789fc2f8b655c99c45820f85
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2016-02-24 00:17:03 +0200

    Binet equation based raytracing works

--- app/Main.hs
+++ app/Main.hs
@@ -5,46 +5,28 @@ module Main where
 import System.Directory
 import Vision.Image
 import Vision.Image.Storage.DevIL
+import Linear hiding (lookAt)
 
-import Geometry
 import Raytracer
 
 myScene :: Scene
-myScene = Scene { stepSize = 0.1
+myScene = Scene { stepSize = 0.16
                 , nSteps = 250
-                , toCartesian = schwarzToCartesian
-                , fromCartesian = cartesianToSchwarz
-                , fgeodesic = schwarzGeodesic
                 , camera = myCamera }
 
-myScene' :: Scene
-myScene' = Scene { stepSize = 0.1
-                 , nSteps = 250
-                 , toCartesian = id
-                 , fromCartesian = id
-                 , fgeodesic = flatGeodesic
-                 , camera = myCamera }
-
 myCamera :: Camera
-myCamera = Camera { position = [1, 0, 20]
-                  , lookAt = [0, 0, 0]
-                  , upVec = [1, 0.2, 0]
+myCamera = Camera { position = V3 0 1 (-20)
+                  , lookAt = V3 0 0 0
+                  , upVec = V3 0.2 1 0
                   , fov = 1.5
-                  , resolution = (400, 225) }
-
-myCamera' :: Camera
-myCamera' = Camera { position = [0, 0, 0]
-                   , lookAt = [1, 0, 0]
-                   , upVec = [0, 0, 1]
-                   , fov = 1.5
-                   , resolution = (800, 450) }
+                  , resolution = (1024, 786) }
 
 main :: IO ()
 main = do
     etex <- load Autodetect "texture.jpg"
     case etex of
         Right (tex :: RGB) -> do
-            img <- computeP $ raytrace myScene tex
+            img <- computeP $ render myScene tex
             removeFile "out.png"
             _ <- save PNG "out.png" ((convert img) :: RGB)
             return ()
--- src/Raytracer.hs
+++ src/Raytracer.hs
@@ -2,84 +2,87 @@
 
 module Raytracer where
 
-import Geometry
 import Vision.Primitive
 import qualified Vision.Image as I
+import Linear hiding (lookAt, mult)
+import qualified Linear as L
+import Control.Lens
 
 data Scene = Scene { stepSize :: Double
                    , nSteps :: Int
-                   , toCartesian :: FourVector -> FourVector
-                   , fromCartesian :: FourVector -> FourVector
-                   , fgeodesic :: FourVector -> FourVector -> FourVector
                    , camera :: Camera }
 
-data Camera = Camera { position :: [Double]
-                     , lookAt :: [Double]
-                     , upVec :: [Double]
+data Camera = Camera { position :: V3 Double
+                     , lookAt :: V3 Double
+                     , upVec :: V3 Double
                      , fov :: Double
                      , resolution :: (Int, Int) }
 
 -- Generate the sight rays ie. initial conditions for the integration
-generateRay :: Scene -> Point -> (FourVector, FourVector)
+generateRay :: Scene -> Point -> (V3 Double, V3 Double)
 {-# INLINE generateRay #-}
-generateRay scn (Z :. y' :. x') = (schwarzInvJacobian posEnd posDiff', posEnd)
+generateRay scn (Z :. y' :. x') = (vel, pos)
     where cam = camera scn
           pos = position cam
-          pos' = fromCartesian scn $ fourPos pos
-          -- Some really sketchy linear algebra stuff
-          -- Rotation matrix for the view vector
-          vfwd = normalize (zipWith (-) (lookAt cam) pos)
-          vleft = normalize (upVec cam `cross` vfwd)
-          vup = vfwd `cross` vleft
-          rotate (u, v, w) = map (\(a, b, c) -> a*u + b*v + c*w)
-              $ zip3 vleft vup vfwd
           xres = (fromIntegral . fst . resolution $ cam) :: Double
           yres = (fromIntegral . snd . resolution $ cam) :: Double
-          posDiff = normalize . rotate
-                    $ ( fov cam * ((fromIntegral x') / xres - 0.5)
-                      , fov cam * ((fromIntegral y') / yres - 0.5) * yres/xres
-                      , 1 )
-          posDiff' = fourVel posDiff
-          posEnd = fromCartesian scn (fourVel $ zipWith (+) pos posDiff)
-          -- Basic linear algebra stuff
-          norm [x, y, z] = sqrt (x*x + y*y + z*z)
-          normalize v@[x, y, z] = let n = norm v in [x/n, y/n, z/n]
-          cross [x, y, z] [u, v, w] = [y*w - z*v, u*z - x*w, x*v - u*y]
-          fourPos [x, y, z] = (0, x, y, z)
-          fourVel [u, v, w] = (1, u, v, w)
+          matr = (L.lookAt pos (lookAt cam) (upVec cam)) ^. _m33
+          vel  = normalize . (matr !*)
+                 $ V3 (fov cam * ((fromIntegral x') / xres - 0.5))
+                      (fov cam * ((fromIntegral y') / yres - 0.5) * yres/xres)
+                      (-1)
 
-raytrace :: Scene -> I.RGB -> I.RGBDelayed
-raytrace scn tex = I.fromFunction sh (uvLookup scn tex . traceRay)
+render :: Scene -> I.RGB -> I.RGBDelayed
+{-# INLINE render #-}
+render scn tex = I.fromFunction (ix2 yres xres) (colorize tex . traceray scn)
     where cam = camera scn
           (xres, yres) = resolution cam
-          sh = ix2 yres xres
-          traceRay = last . take (nSteps scn)
-                     . iterate (rk4 (stepSize scn) (fgeodesic scn))
-                     . generateRay scn
 
-clamp :: (Ord a) => a -> a -> a -> a
-clamp mn mx = max mn . min mx
+sqrnorm :: V3 Double -> Double
+{-# INLINE sqrnorm #-}
+sqrnorm (V3 !x !y !z) = x*x + y*y + z*z
 
-uvLookup :: Scene -> I.RGB -> (FourVector, FourVector) -> I.RGBPixel
-uvLookup scn tex (vel, pos) = tex `I.index`
-                            (ix2 (clamp 0 (h-1) . floor $ v * (fromIntegral h))
-                                 (clamp 0 (w-1) . floor $ u * (fromIntegral w)))
+traceray :: Scene -> Point -> (V3 Double, V3 Double)
+{-# INLINE traceray #-}
+traceray !scn !pt = last . take (nSteps scn)
+                  . takeWhile (\(_, p) -> sqrnorm p > 1)
+                  . iterate (rk4 (stepSize scn) (fgeodesic h2)) $ ray
+                  where ray@(vel, pos) = generateRay scn pt
+                        h2 = sqrnorm (pos `cross` vel)
+
+colorize :: I.RGB -> (V3 Double, V3 Double) -> I.RGBPixel
+{-# INLINE colorize #-}
+colorize tex (!vel, !pos) = if sqrnorm pos < 2.25 then I.RGBPixel 0 0 0
+                                                  else uvLookup tex vel
+
+uv :: V3 Double -> (Double, Double)
+{-# INLINE uv #-}
+uv vec@(V3 !x !y !z) = (u, v) where
+    u = 0.5 + (atan2 y x) / (2*pi)
+    v = 0.5 - asin (z/r) / pi
+    r = norm vec
+
+uvLookup :: I.RGB -> V3 Double -> I.RGBPixel
+{-# INLINE uvLookup #-}
+uvLookup !tex !vel = tex `I.index`
+                      (ix2 (floor $ v * (fromIntegral h))
+                           (floor $ u * (fromIntegral w)))
     where (Z :. h :. w) = I.shape tex
-          u = 0.5 + (atan2 y x) / (2*pi)
-          v = 0.5 - asin (z/r) / pi
-          (_, x, y, z) = schwarzJacobian pos vel
-          r = sqrt (x*x + y*y + z*z)
+          (u, v) = uv vel
 
-rk4 :: Double -> (FourVector -> FourVector -> FourVector)
-       -> (FourVector, FourVector) -> (FourVector, FourVector)
+rk4 :: Double -> ((V3 Double, V3 Double) -> (V3 Double, V3 Double))
+       -> (V3 Double, V3 Double) -> (V3 Double, V3 Double)
 {-# INLINE rk4 #-}
-rk4 !h !f !y = y `add'`
-    ((k1 `add'` (k2 `mult'` 2) `add'` (k3 `mult'` 2) `add'` k4) `mult'` (h/6))
-    where k1 = f' y
-          k2 = f' (y `add'` (k1 `mult'` (h/2)))
-          k3 = f' (y `add'` (k2 `mult'` (h/2)))
-          k4 = f' (y `add'` (k3 `mult'` h))
+rk4 !h !f !y = y `add`
+    ((k1 `add` (k2 `mul` 2) `add` (k3 `mul` 2) `add` k4) `mul` (h/6))
+    where k1 = f y
+          k2 = f (y `add` (k1 `mul` (h/2)))
+          k3 = f (y `add` (k2 `mul` (h/2)))
+          k4 = f (y `add` (k3 `mul` h))
 
-          f' (!v, !c) = (f v c, v)
-          add' (!x, !z) (!u, !v) = (x `add` u, z `add` v)
-          mult' (!u, !v) !a = (u `mult` a, v `mult` a)
+          add (!x, !z) (!u, !v) = (x ^+^ u, z ^+^ v)
+          mul (!u, !v) !a = (u ^* a, v ^* a)
+
+fgeodesic :: Double -> (V3 Double, V3 Double) -> (V3 Double, V3 Double)
+{-# INLINE fgeodesic #-}
+fgeodesic h2 (!vel, !pos) = (-1.5*h2 / ((sqrnorm pos)**2.5) *^ pos, vel)
