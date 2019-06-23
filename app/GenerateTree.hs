728f864c45b407e6ec8b4b953a4c683a08893822
Sakari Kapanen <sakari@kapanen.xyz>
2019-06-24 00:41:05 +0300

    Proper argument parsing for GenerateTree

--- app/GenerateTree.hs
+++ app/GenerateTree.hs
@@ -1,29 +1,47 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -fno-cse #-}
+
 module Main where
 
 import qualified Data.ByteString.Lazy as B
-import System.Environment (getArgs)
+import System.Console.CmdArgs
 
 import Util
 import StarMap
+
+data GenerateTree = GenerateTree { infile :: FilePath
+                                 , outfile :: FilePath
+                                 , division :: Int }
+                                 deriving (Show, Data, Typeable)
+
+argparser :: GenerateTree
+argparser = GenerateTree { infile = def
+                             &= typ "INPUTFILE"
+                             &= argPos 0
+                         , outfile = def
+                             &= typ "OUTPUTFILE"
+                             &= argPos 1
+                         , division = 20
+                             &= help "division of the search space"
+                             &= opt (20 :: Int)
+                         } &= summary "GenerateTree utility v0.1"
+                           &= program "generate-tree"
 
 -- Generate and store the k-d star tree from a star catalog
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        [infile, outfile] -> do
-            outfile' <- normalizePath outfile
-            infile' <- normalizePath infile
-            eitherMap <- readMapFromFile infile'
-            case eitherMap of
-                Right stars -> do
-                    putStrLn "Generating the star tree..."
-                    tree <- timeAction "Building the tree"
-                        $ assembleStarGrid 10 stars
-                    let treeBl = B.fromStrict $ treeToByteString tree
-                    promptOverwriteFile outfile'
-                        (\filename -> B.writeFile filename treeBl)
-                    putStrLn $ "Tree saved to " ++ outfile' ++ "."
-                Left  err   ->  putStrLn err
-        _ -> putStrLn "Usage: generate-tree <INFILE> <OUTFILE>"
+    cmdline <- cmdArgs argparser
+    outfile' <- normalizePath $ outfile cmdline
+    infile' <- normalizePath $ infile cmdline
+    eitherMap <- readMapFromFile infile'
+    case eitherMap of
+        Right stars -> do
+            putStrLn "Generating the star tree..."
+            tree <- timeAction "Building the tree"
+                $ assembleStarGrid (division cmdline) stars
+            let treeBl = B.fromStrict $ treeToByteString tree
+            promptOverwriteFile outfile'
+                (\filename -> B.writeFile filename treeBl)
+            putStrLn $ "Tree saved to " ++ outfile' ++ "."
+        Left  err   ->  putStrLn err
